417f21c88b37d5395c6c64743db9fc967585865a
ofgonen-home <ofgonen@gmail.com>
2022-12-12 17:37:01 +0300

    Implement RopeData

---
+++ Assets/Script/RopeData.cs
@@ -0,0 +1,20 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+[ CreateAssetMenu( fileName = "shared_rope_data", menuName = "FF/Game/Rope Data" ) ]
+public class RopeData : ScriptableObject
+{
+#region Fields
+  [ Title( "Setup" ) ]
+    [ LabelText( "Rope Reach Length" ), SerializeField ] int rope_length;
+    [ LabelText( "Rope Damage" ), SerializeField ] float rope_damage;
+    [ LabelText( "Rope Launch Delay" ), SerializeField ] float rope_delay;
+    [ LabelText( "Rope Launch Duration" ), SerializeField ] float rope_duration_reach;
+    [ LabelText( "Rope Return Duration" ), SerializeField ] float rope_duration_return;
+#endregion
+}
