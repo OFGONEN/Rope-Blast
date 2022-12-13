f2f9739ba0c3bacb912461c375d1e2a237907117
ofgonen-home <ofgonen@gmail.com>
2022-12-13 17:12:37 +0300

    RopeData: Add delay to Launch and Return

--- Assets/Script/RopeData.cs
+++ Assets/Script/RopeData.cs
@@ -14,15 +14,17 @@ public class RopeData : ScriptableObject
     [ LabelText( "Rope Level" ), SerializeField ] int rope_level;
     [ LabelText( "Rope Reach Length" ), SerializeField ] int rope_length;
     [ LabelText( "Rope Damage" ), SerializeField ] float rope_damage;
-    [ LabelText( "Rope Launch Delay" ), SerializeField ] float rope_delay;
-    [ LabelText( "Rope Launch Duration" ), SerializeField ] float rope_duration_reach;
-    [ LabelText( "Rope Return Duration" ), SerializeField ] float rope_duration_return;
+    [ LabelText( "Rope Launch Delay" ), SerializeField ] float rope_reach_delay;
+    [ LabelText( "Rope Launch Duration" ), SerializeField ] float rope_reach_duration;
+    [ LabelText( "Rope Return Delay" ), SerializeField ] float rope_return_delay;
+    [ LabelText( "Rope Return Duration" ), SerializeField ] float rope_return_duration;
 
     public int RopeLevel            => rope_level;
     public int RopeLength           => rope_length;
     public float RopeDamage         => rope_damage;
-    public float RopeDelay          => rope_delay;
-    public float RopeDurationReach  => rope_duration_reach;
-    public float RopeDurationReturn => rope_duration_return;
+    public float RopeReachDelay     => rope_reach_delay;
+    public float RopeReachDuration  => rope_reach_duration;
+    public float RopeReturnDelay    => rope_return_delay;
+    public float RopeReturnDuration => rope_return_duration;
 #endregion
 }
