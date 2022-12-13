b1585c6600dd22cb490a07f15b471d8cf589f8d9
ofgonen-home <ofgonen@gmail.com>
2022-12-13 13:23:44 +0300

    Implement RopeBox

--- Assets/Script/RopeBox.cs
+++ Assets/Script/RopeBox.cs
@@ -3,10 +3,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using FFStudio;
+using TMPro;
+using Sirenix.OdinInspector;
 
 public class RopeBox : MonoBehaviour
 {
 #region Fields
+  [ Title( "Setup" ) ]
+    [ LabelText( "Rope Box Pool" ), SerializeField ] Pool_RopeBox pool_ropeBox;
+
+  [ Title( "Components" ) ]
+    [ LabelText( "Rope Box Renderer" ) ] Renderer _renderer;
+    [ LabelText( "Rope Box Mesh Filter" ) ] MeshFilter _meshFilter;
+    // [ LabelText( "Rope Box UI Text" ) ] TextMeshProUGUI _textRenderer;
+    // [ LabelText( "Rope Box UI Image" ) ] Image _imageRenderer;
+
+    public RopeBoxData RopeBoxData => ropeBox_data;
+// Private
+    RopeBoxData ropeBox_data;
 #endregion
 
 #region Properties
@@ -16,9 +32,30 @@ public class RopeBox : MonoBehaviour
 #endregion
 
 #region API
+    public void Spawn( RopeBoxData ropeBoxData, Vector3 position )
+    {
+		gameObject.SetActive( true );
+		transform.position = position;
+
+		ropeBox_data = ropeBoxData;
+	}
+
+    public void DeSpawn()
+    {
+		pool_ropeBox.ReturnEntity( this );
+	}
 #endregion
 
 #region Implementation
+    void ConvertRopeBox()
+    {
+		_meshFilter.mesh         = ropeBox_data.RopeBoxMesh;
+		_renderer.sharedMaterial = ropeBox_data.RopeBoxMaterial;
+
+        //todo: Enable this after visual design is determined.
+		// _textRenderer.text   = "Lvl " + ropeBox_data.RopeData.RopeLevel;
+		// _imageRenderer.color = ropeBox_data.RopeBoxUIColor;
+	}
 #endregion
 
 #region Editor Only
