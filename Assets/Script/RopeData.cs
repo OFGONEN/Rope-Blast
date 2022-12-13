c4c2a8d61e58aab10cfd8b2f156c103d9c5b5e0c
ofgonen-home <ofgonen@gmail.com>
2022-12-13 18:15:17 +0300

    RopeData: Add Ease settings & refactor duration settings to speed settings

--- Assets/Script/RopeData.cs
+++ Assets/Script/RopeData.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
 using Sirenix.OdinInspector;
+using DG.Tweening;
 
 [ CreateAssetMenu( fileName = "shared_rope_data", menuName = "FF/Game/Rope Data" ) ]
 public class RopeData : ScriptableObject
@@ -14,17 +15,21 @@ public class RopeData : ScriptableObject
     [ LabelText( "Rope Level" ), SerializeField ] int rope_level;
     [ LabelText( "Rope Reach Length" ), SerializeField ] int rope_length;
     [ LabelText( "Rope Damage" ), SerializeField ] float rope_damage;
-    [ LabelText( "Rope Launch Delay" ), SerializeField ] float rope_reach_delay;
-    [ LabelText( "Rope Launch Duration" ), SerializeField ] float rope_reach_duration;
+    [ LabelText( "Rope Launch Delay" ), SerializeField ] float rope_launch_delay;
+    [ LabelText( "Rope Launch Speed" ), SerializeField ] float rope_launch_speed;
+    [ LabelText( "Rope Launch Ease" ), SerializeField ] Ease rope_launch_ease;
     [ LabelText( "Rope Return Delay" ), SerializeField ] float rope_return_delay;
-    [ LabelText( "Rope Return Duration" ), SerializeField ] float rope_return_duration;
+    [ LabelText( "Rope Return Speed" ), SerializeField ] float rope_return_speed;
+    [ LabelText( "Rope Return Ease" ), SerializeField ] Ease rope_return_ease;
 
     public int RopeLevel            => rope_level;
     public int RopeLength           => rope_length;
     public float RopeDamage         => rope_damage;
-    public float RopeReachDelay     => rope_reach_delay;
-    public float RopeReachDuration  => rope_reach_duration;
+    public float RopeLaunchDelay    => rope_launch_delay;
+    public float RopeLaunchSpeed    => rope_launch_speed;
+    public Ease RopeLaunchEase      => rope_launch_ease;
     public float RopeReturnDelay    => rope_return_delay;
-    public float RopeReturnDuration => rope_return_duration;
+    public float RopeReturnSpeed    => rope_return_speed;
+    public Ease RopeReturnEase      => rope_return_ease;
 #endregion
 }
