8c212cb857b06389b1761a8f12f5d9a493350941
ofgonen_office <ofgonen@gmail.com>
2022-12-14 18:01:41 +0300

    SelectionSystem: Implement system delegates

---
+++ Assets/Script/SelectionSystem.cs
@@ -0,0 +1,76 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+[ CreateAssetMenu( fileName = "system_selection", menuName = "FF/Game/System/Selection" ) ]
+public class SelectionSystem : ScriptableObject
+{
+#region Fields
+  [ Title( "Setup" ) ]
+    [ LabelText( "Main Camera Reference" ), SerializeField ] SharedReferenceNotifier notif_camera_reference;
+
+    Camera camera_main;
+
+    UnityMessage onFingerDown;
+    UnityMessage onFingerUp;
+    UnityMessage onUpdate;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    //Info: EditorCall
+    public void OnLevelRevealed()
+    {
+        camera_main = ( notif_camera_reference.sharedValue as Transform ).GetComponent< Camera >();
+
+		onFingerDown = TryToSelectSlot;
+	}
+
+    //Info: EditorCall
+    public void OnFingerDown()
+    {
+		onFingerDown();
+	}
+
+    //Info: EditorCall
+    public void OnFingerUp()
+    {
+		onFingerUp();
+	}
+
+    //Info: Call on manager_asset Update 
+    public void OnUpdate()
+    {
+		onUpdate();
+	}
+
+    //Info: Call on manager_asset Awake 
+    public void EmptyDelegates()
+    {
+		onFingerDown = ExtensionMethods.EmptyMethod;
+		onFingerUp   = ExtensionMethods.EmptyMethod;
+		onUpdate     = ExtensionMethods.EmptyMethod;
+	}
+#endregion
+
+#region Implementation
+    void TryToSelectSlot()
+    {
+
+    }
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
