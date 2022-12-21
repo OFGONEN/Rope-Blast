d6586b34fc86114f179277d3ae66db9df9fef6dc
ofgonen-home <ofgonen@gmail.com>
2022-12-21 20:15:45 +0300

    TileTable: Level complete logic

--- Assets/Script/TileTable.cs
+++ Assets/Script/TileTable.cs
@@ -10,22 +10,32 @@ using Sirenix.OdinInspector;
 public class TileTable : MonoBehaviour
 {
 #region Fields
-    int movement_count;
+	[ SerializeField ] GameEvent event_level_complete;
 
-    RecycledTween recycledTween = new RecycledTween();
+    int movement_count_total;
+    int movement_count_current;
+
+	int tileRow_count;
+
+	RecycledTween recycledTween = new RecycledTween();
 #endregion
 
 #region Properties
 #endregion
 
 #region Unity API
+	private void Awake()
+	{
+		tileRow_count = transform.childCount;
+	}
 #endregion
 
 #region API
 	//Info: Editor Call
     public void OnTableMove()
     {
-		movement_count += 1;
+		movement_count_total   += 1;
+		movement_count_current += 1;
 
         if( !recycledTween.IsPlaying() )
 			MoveTable();
@@ -35,21 +45,23 @@ public class TileTable : MonoBehaviour
 #region Implementation
     void OnTableMoveComplete()
     {
-        if( movement_count > 0 )
+        if( movement_count_current > 0 )
 			MoveTable();
+		else if( movement_count_total == tileRow_count )
+			event_level_complete.Raise();
 	}
 
     void MoveTable()
     {
 		recycledTween.Recycle(
-			transform.DOMove( movement_count * GameSettings.Instance.tile_table_movement_delta * -transform.forward,
+			transform.DOMove( movement_count_current * GameSettings.Instance.tile_table_movement_delta * -transform.forward,
 			GameSettings.Instance.tile_table_movement_duration )
 			.SetRelative()
 			.SetEase( GameSettings.Instance.tile_table_movement_ease ),
 			OnTableMoveComplete
 		);
 
-		movement_count = 0;
+		movement_count_current = 0;
     }
 #endregion
 
