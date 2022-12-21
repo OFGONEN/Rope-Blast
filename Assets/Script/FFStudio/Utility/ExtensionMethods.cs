8912aa18bdd62cba9516362f8909d3ce3469971d
ofgonen-home <ofgonen@gmail.com>
2022-12-21 20:32:58 +0300

    ExtensionMethods: Purchase Count Key

--- Assets/Script/FFStudio/Utility/ExtensionMethods.cs
+++ Assets/Script/FFStudio/Utility/ExtensionMethods.cs
@@ -12,7 +12,8 @@ namespace FFStudio
 	public static class ExtensionMethods
 	{
 		public static readonly string SAVE_PATH    = Application.persistentDataPath + "/Saves/";
-		public static readonly string Key_Currency = "Currency";
+		public static readonly string Key_Currency = "currency";
+		public static readonly string Key_Purchase_Count = "purchase_count";
 
 		static List< Transform > baseModelBones   = new List< Transform >( 96 );
 		static List< Transform > targetModelBones = new List< Transform >( 96 );
