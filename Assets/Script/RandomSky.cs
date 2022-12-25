4fc40e69e3cf7a29baff0d7e6bcd886a01946d1d
ofgonen_office <ofgonen@gmail.com>
2022-12-25 03:26:31 +0300

    Implement RandomSky

---
+++ Assets/Script/RandomSky.cs
@@ -0,0 +1,35 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Imphenzia;
+
+public class RandomSky : MonoBehaviour
+{
+#region Fields
+    [ SerializeField ] GradientSkyObject gradientSkyObject;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+    void Start()
+    {
+		gradientSkyObject.gradient = GameSettings.Instance.sky_gradient_array.ReturnRandom();
+	}
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
